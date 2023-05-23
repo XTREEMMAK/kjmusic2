d3de404d661d63c92f753d9474a80d4cd243d67d
XTREEMMAK <get@jamaale.live>
2023-05-23 16:18:56 -0400

    Music player XML fixes.

--- src/public/js/mp3-sticky-player/player_settings.js
+++ src/public/js/mp3-sticky-player/player_settings.js
@@ -1,4 +1,4 @@
-const host = 'https://cdn.music.keyjayhd.com';
+const host = window.location.href.match(/^.*\//);
 const playlists = [   
     {
         genre: "STARTER",
@@ -54,7 +54,7 @@ var playlistHTML = '';
 
 playlists.forEach(element =>{
     playlistHTML += `
-    <li data-source="./js/mp3-sticky-player/content/playlists/`+element.genre+`.xml" data-playlist-name="`+element.genre+`" data-thumbnail-path="./js/mp3-sticky-player/content/playlists/thumbs/`+element.genre+`.jpg">
+    <li data-source="`+host+`/js/mp3-sticky-player/content/playlists/`+element.genre+`.xml" data-playlist-name="`+element.genre+`" data-thumbnail-path="`+host+`/js/mp3-sticky-player/content/playlists/thumbs/`+element.genre+`.jpg">
                 <p class="fwdmsp-categories-title"><span class="fwdmsp-header">Title: </span><span class="fwdmsp-title">`+element.display_title+`</span></p>
                 <p class="fwdmsp-categories-type"><span class="fwdmsp-header">Type: </span>`+element.playlist_type+`</p>
                 <p class="fwdmsp-categories-description"><span class="fwdmsp-header">Description: </span>`+element.description+`</p>
