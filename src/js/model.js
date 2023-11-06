cea5381a9e879e925b4d036143c97a14873a322c
XTREEMMAK <get@jamaale.live>
2023-11-06 14:24:40 -0500

    Updating Support Page to Production Page.

--- src/js/model.js
+++ src/js/model.js
@@ -177,24 +177,25 @@ window.get_contact = function(){
   modal.open();
 }
 
-//Support Page------------------
+//Production Page------------------
 window.get_support = function(){
-  modal.setContent(
-      `<div class="support_modal_wrapper">
-              <div class="sm_pic_box" style="width: 40%; margin-left: 5px; margin-right: 5px;"><img src="`+main.CDN+`/img/pc_supp_left.webp" style="width: 100%;"/></div>
-              <div class="sm_content_box">
-                  <h1>More Key Jay, bigger Productions</h1>
-                  <h3>Support the development of even larger KEY JAY PRODUCTIONS <br/> by:</h3>
-                  <div class="sp_options_wrapper">
-                      <div class="sp_option sp_option_rightb"><p>Directly supporting KEY JAY by becoming a POWER CREW VIP. Your support goes farther this way by avoiding unnecessary platform fees.</p><a class="pcsp_link" href="https://keyjayonline.com/powercrew.html" target="_blank"><img src="`+main.CDN+`/img/pc_logo_small.png" alt="Join the POWER CREW VIP"/></a></div>
-                      <div class="sp_option"><p>Or by Supporting KEY JAY on these other fine platforms <br/>(One-Time or by Subscription options available)</p><!--<img src="`+main.CDN+`/img/patreon-logo.jpg" style="width: 250px;" alt="KEY JAY's Patreon"/>--><a href="https://ko-fi.com/keyjayhd" target="_blank"><img src="`+main.CDN+`/img/kofi_logo.png" style="width: 289px;" alt="KEY JAY's Ko-Fi"/></a><a href="https://paypal.me/keyjayproductions?country.x=US&locale.x=en_US" target="_blank"><img src="`+main.CDN+`/img/pp_logo.png" style="width: 289px;" alt="KEY JAY's Pay Pal"/></a>
-                      
-                      </div>
-                  </div>
-              </div>
-          </div>`
-      );
-  modal.open();
+  window.open("https://keyayproductions.com", "_blank")
+//  modal.setContent(
+//      `<div class="support_modal_wrapper">
+//              <div class="sm_pic_box" style="width: 40%; margin-left: 5px; margin-right: 5px;"><img src="`+main.CDN+`/img/pc_supp_left.webp" style="width: 100%;"/></div>
+//              <div class="sm_content_box">
+//                  <h1>More Key Jay, bigger Productions</h1>
+//                  <h3>Support the development of even larger KEY JAY PRODUCTIONS <br/> by:</h3>
+//                  <div class="sp_options_wrapper">
+//                      <div class="sp_option sp_option_rightb"><p>Directly supporting KEY JAY by becoming a POWER CREW VIP. Your support goes farther this way by avoiding unnecessary platform fees.</p><a class="pcsp_link" href="https://keyjayonline.com/powercrew.html" target="_blank"><img src="`+main.CDN+`/img/pc_logo_small.png" alt="Join the POWER CREW VIP"/></a></div>
+//                      <div class="sp_option"><p>Or by Supporting KEY JAY on these other fine platforms <br/>(One-Time or by Subscription options available)</p><!--<img src="`+main.CDN+`/img/patreon-logo.jpg" style="width: 250px;" alt="KEY JAY's Patreon"/>--><a href="https://ko-fi.com/keyjayhd" target="_blank"><img src="`+main.CDN+`/img/kofi_logo.png" style="width: 289px;" alt="KEY JAY's Ko-Fi"/></a><a href="https://paypal.me/keyjayproductions?country.x=US&locale.x=en_US" target="_blank"><img src="`+main.CDN+`/img/pp_logo.png" style="width: 289px;" alt="KEY JAY's Pay Pal"/></a>
+//                      
+//                      </div>
+//                  </div>
+//              </div>
+//          </div>`
+//      );
+//  modal.open();
 }
 
 function get_backgrounds() {
